7fa0e0a0b0e3050a66551840e946b9f0019a7040
Kevin20201 <kevin.yu.tu@gmail.com>
2021-05-03 13:24:55 -0700

    Added Select_And

--- select_test.hpp
+++ select_test.hpp
@@ -5,6 +5,7 @@
 #include "spreadsheet.hpp"
 #include "select_contains.hpp"
 #include "select_not.hpp"
+#include "select_and.hpp"
 #include "select_mock.hpp"
 using namespace std;
 
@@ -184,5 +185,127 @@ TEST(NotTest, BlankInRowTest) {
 
     EXPECT_EQ(n.select(&sheet, 0), true);
 
+}
+
+//Select_And Tests
+TEST(AndTest, MockTrueTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","","7","Brown"});
+    Select_Mock_True * mockTrue1 = new Select_Mock_True();
+    Select_Mock_True * mockTrue2 = new Select_Mock_True();
+    Select_And a(mockTrue1, mockTrue2);
+    
+    EXPECT_EQ(a.select(&sheet, 0), true);
+}
+
+TEST(AndTest, MockFalseTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","","7","Brown"});
+    Select_Mock_False * mockFalse1 = new Select_Mock_False();
+    Select_Mock_False * mockFalse2 = new Select_Mock_False();
+    Select_And a(mockFalse1, mockFalse2);
+    
+    EXPECT_EQ(a.select(&sheet, 0), false);
+}
+
+TEST(AndTest, MockTrueFalseTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","","7","Brown"});
+    Select_Mock_True * mockTrue = new Select_Mock_True();
+    Select_Mock_False * mockFalse = new Select_Mock_False();
+    Select_And a(mockTrue, mockFalse);
+    
+    EXPECT_EQ(a.select(&sheet, 0), false);
+}
+
+TEST(AndTest, MockFalseTrueTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","","7","Brown"});
+    Select_Mock_True * mockTrue = new Select_Mock_True();
+    Select_Mock_False * mockFalse = new Select_Mock_False();
+    Select_And a(mockFalse, mockTrue);
+    
+    EXPECT_EQ(a.select(&sheet, 0), false);
+}
+
+TEST(AndTest, GeneralTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","Dog","7","Brown"});
+    sheet.add_row({"Cocoa","Dog","12","Black"});
+    sheet.add_row({"Casper","Cat","8","White"});
+    sheet.add_row({"Lucky","Cat","2","Gray"});
+    Select_And a(new Select_Contains(&sheet, "Species", "Dog"), new Select_Contains(&sheet, "Color", "Black"));
+
+    EXPECT_EQ(a.select(&sheet, 0), false);
+    EXPECT_EQ(a.select(&sheet, 1), true);
+    EXPECT_EQ(a.select(&sheet, 2), false);
+    EXPECT_EQ(a.select(&sheet, 3), false);
+}
+
+TEST(AndTest, CapitalTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","dog","7","Brown"});
+    sheet.add_row({"Cocoa","dog","12","Black"});
+    Select_And a(new Select_Contains(&sheet, "Species", "Dog"), new Select_Contains(&sheet, "Color", "black"));
+
+    EXPECT_EQ(a.select(&sheet, 0), false);
+    EXPECT_EQ(a.select(&sheet, 1), false);
+
+}
+
+TEST(AndTest, BadCategoryTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","Dog","7","Brown"});
+    sheet.add_row({"Cocoa","Dog","12","Black"});
+    sheet.add_row({"Casper","Cat","8","White"});
+    sheet.add_row({"Lucky","Cat","2","Gray"});
+    Select_And a(new Select_Contains(&sheet, "Animal", "Horse"), new Select_Contains(&sheet, "Color", "Black"));
+
+    EXPECT_EQ(a.select(&sheet, 0), false);
+    EXPECT_EQ(a.select(&sheet, 1), false);
+    EXPECT_EQ(a.select(&sheet, 2), false);
+    EXPECT_EQ(a.select(&sheet, 3), false);
+}
+
+TEST(AndTest, ContainsSubstringTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","Dog","7","Brown"});
+    sheet.add_row({"Cocoa","Dog","12","Black"});
+    Select_And a(new Select_Contains(&sheet, "Species", "og"), new Select_Contains(&sheet, "Color", "B"));
+
+    EXPECT_EQ(a.select(&sheet, 0), true);
+    EXPECT_EQ(a.select(&sheet, 1), true);
+}
+
+TEST(AndTest, ContainsBlankTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","Dog","7","Brown"});
+    sheet.add_row({"Cocoa","Dog","12","Black"});
+    sheet.add_row({"Casper","Cat","8","White"});
+    sheet.add_row({"Lucky","Cat","2","Gray"});
+    Select_And a(new Select_Contains(&sheet, "Species", ""), new Select_Contains(&sheet, "", "Black"));
+
+    EXPECT_EQ(a.select(&sheet, 0), false);
+    EXPECT_EQ(a.select(&sheet, 1), false);
+    EXPECT_EQ(a.select(&sheet, 2), false);
+    EXPECT_EQ(a.select(&sheet, 3), false);
+}
+
+TEST(AndTest, BlankInRowTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","","7","Brown"});
+    Select_And a(new Select_Contains(&sheet, "Species", "Dog"), new Select_Contains(&sheet, "Color", "Black"));
+
+    EXPECT_EQ(a.select(&sheet, 0), false);
 }
 #endif //__SELECT_TEST_HPP__
