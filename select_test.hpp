689b3ed28afb50ce54c6be1f386fbc326065dce3
Kevin20201 <kevin.yu.tu@gmail.com>
2021-05-03 13:46:58 -0700

    Added Select_Or

--- select_test.hpp
+++ select_test.hpp
@@ -6,6 +6,7 @@
 #include "select_contains.hpp"
 #include "select_not.hpp"
 #include "select_and.hpp"
+#include "select_or.hpp"
 #include "select_mock.hpp"
 using namespace std;
 
@@ -307,5 +308,127 @@ TEST(AndTest, BlankInRowTest) {
     Select_And a(new Select_Contains(&sheet, "Species", "Dog"), new Select_Contains(&sheet, "Color", "Black"));
 
     EXPECT_EQ(a.select(&sheet, 0), false);
+}
+
+//Select_Or Tests
+TEST(OrTest, MockTrueTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","","7","Brown"});
+    Select_Mock_True * mockTrue1 = new Select_Mock_True();
+    Select_Mock_True * mockTrue2 = new Select_Mock_True();
+    Select_Or o(mockTrue1, mockTrue2);
+    
+    EXPECT_EQ(o.select(&sheet, 0), true);
+}
+
+TEST(OrTest, MockFalseTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","","7","Brown"});
+    Select_Mock_False * mockFalse1 = new Select_Mock_False();
+    Select_Mock_False * mockFalse2 = new Select_Mock_False();
+    Select_Or o(mockFalse1, mockFalse2);
+    
+    EXPECT_EQ(o.select(&sheet, 0), false);
+}
+
+TEST(OrTest, MockTrueFalseTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","","7","Brown"});
+    Select_Mock_True * mockTrue = new Select_Mock_True();
+    Select_Mock_False * mockFalse = new Select_Mock_False();
+    Select_Or o(mockTrue, mockFalse);
+    
+    EXPECT_EQ(o.select(&sheet, 0), true);
+}
+
+TEST(OrTest, MockFalseTrueTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","","7","Brown"});
+    Select_Mock_True * mockTrue = new Select_Mock_True();
+    Select_Mock_False * mockFalse = new Select_Mock_False();
+    Select_Or o(mockFalse, mockTrue);
+    
+    EXPECT_EQ(o.select(&sheet, 0), true);
+}
+
+TEST(OrTest, GeneralTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","Dog","7","Brown"});
+    sheet.add_row({"Cocoa","Dog","12","Black"});
+    sheet.add_row({"Casper","Cat","8","White"});
+    sheet.add_row({"Lucky","Cat","2","Gray"});
+    Select_Or o(new Select_Contains(&sheet, "Species", "Dog"), new Select_Contains(&sheet, "Color", "Black"));
+
+    EXPECT_EQ(o.select(&sheet, 0), true);
+    EXPECT_EQ(o.select(&sheet, 1), true);
+    EXPECT_EQ(o.select(&sheet, 2), false);
+    EXPECT_EQ(o.select(&sheet, 3), false);
+}
+
+TEST(OrTest, CapitalTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","dog","7","Brown"});
+    sheet.add_row({"Cocoa","dog","12","Black"});
+    Select_Or o(new Select_Contains(&sheet, "Species", "Dog"), new Select_Contains(&sheet, "Color", "black"));
+
+    EXPECT_EQ(o.select(&sheet, 0), false);
+    EXPECT_EQ(o.select(&sheet, 1), false);
+
+}
+
+TEST(OrTest, BadCategoryTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","Dog","7","Brown"});
+    sheet.add_row({"Cocoa","Dog","12","Black"});
+    sheet.add_row({"Casper","Cat","8","White"});
+    sheet.add_row({"Lucky","Cat","2","Gray"});
+    Select_Or o(new Select_Contains(&sheet, "Animal", "Horse"), new Select_Contains(&sheet, "Color", "Black"));
+
+    EXPECT_EQ(o.select(&sheet, 0), false);
+    EXPECT_EQ(o.select(&sheet, 1), true);
+    EXPECT_EQ(o.select(&sheet, 2), false);
+    EXPECT_EQ(o.select(&sheet, 3), false);
+}
+
+TEST(OrTest, ContainsSubstringTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","Dog","7","Brown"});
+    sheet.add_row({"Cocoa","Dog","12","Black"});
+    Select_Or o(new Select_Contains(&sheet, "Species", "og"), new Select_Contains(&sheet, "Color", "B"));
+
+    EXPECT_EQ(o.select(&sheet, 0), true);
+    EXPECT_EQ(o.select(&sheet, 1), true);
+}
+
+TEST(OrTest, ContainsBlankTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","Dog","7","Brown"});
+    sheet.add_row({"Cocoa","Dog","12","Black"});
+    sheet.add_row({"Casper","Cat","8","White"});
+    sheet.add_row({"Lucky","Cat","2","Gray"});
+    Select_Or o(new Select_Contains(&sheet, "Species", ""), new Select_Contains(&sheet, "", "Black"));
+
+    EXPECT_EQ(o.select(&sheet, 0), true);
+    EXPECT_EQ(o.select(&sheet, 1), true);
+    EXPECT_EQ(o.select(&sheet, 2), true);
+    EXPECT_EQ(o.select(&sheet, 3), true);
+}
+
+TEST(OrTest, BlankInRowTest) {
+    Spreadsheet sheet;
+    sheet.set_column_names({"Name","Species","Age","Color"});
+    sheet.add_row({"Hershey","","7","Brown"});
+    Select_Or o(new Select_Contains(&sheet, "Species", "Dog"), new Select_Contains(&sheet, "Color", "Black"));
+
+    EXPECT_EQ(o.select(&sheet, 0), false);
 }
 #endif //__SELECT_TEST_HPP__
